648cf1646809c1eea7929c9b4dcc759a04c95381
CodeBy-Icker <shagyalba@gmail.com>
2026-04-18 00:19:02 -0600

    Create P03promedio.py

---
+++ P03promedio.py
@@ -0,0 +1,9 @@
+#Icker Alba
+# Nombre: Icker Santiago
+cal1 = float(input("Ingresa calificación 1: "))
+cal2 = float(input("Ingresa calificación 2: "))
+cal3 = float(input("Ingresa calificación 3: "))
+
+promedio = (cal1 + cal2 + cal3) / 3
+
+print("Promedio:", promedio)
