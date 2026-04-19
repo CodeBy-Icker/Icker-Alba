c5c7862958df09eeb49c0fb99792e6b542cf7b37
CodeBy-Icker <shagyalba@gmail.com>
2026-04-19 16:28:17 -0600

    Add turtle graphics script to draw colorful stars

---
+++ P09ScripsGraficos.py
@@ -0,0 +1,24 @@
+#Icker Alba
+import turtle
+import random
+ 
+# Configuración
+pantalla = turtle.Screen()
+pantalla.title("Estrella de Colores - Turtle")
+pantalla.bgcolor("black")
+pantalla.setup(width=600, height=600)
+ 
+lapiz = turtle.Turtle()
+lapiz.speed(0)
+lapiz.width(2)
+ 
+colores = ["red", "orange", "yellow", "green", "cyan", "blue", "violet", "magenta"]
+ 
+# Dibujar espiral de estrellas
+for i in range(200):
+    lapiz.color(colores[i % len(colores)])
+    lapiz.forward(i * 0.5)
+    lapiz.right(91)
+ 
+lapiz.hideturtle()
+pantalla.mainloop()
