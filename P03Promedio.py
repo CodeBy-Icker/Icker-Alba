20a77b5cbd27ec600599df1ae0deb3008d8e1bd9
CodeBy-Icker <shagyalba@gmail.com>
2026-04-19 20:17:37 -0600

    Update P03Promedio.py

--- P03Promedio.py
+++ P03Promedio.py
@@ -1,4 +1,4 @@
-#Icker Alba
+#Icker Santiago Alba Ayala
 cal1 = float(input("Ingresa calificación 1: "))
 cal2 = float(input("Ingresa calificación 2: "))
 cal3 = float(input("Ingresa calificación 3: "))
